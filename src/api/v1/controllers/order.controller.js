81d8825a17839db1eb458f1ae96b24388b0d442e
Ruchira Dilshan Wijerathna <ruchiradilshan17@gmail.com>
2025-03-03 12:47:29 +0530

    get order in order controller

--- src/api/v1/controllers/order.controller.js
+++ src/api/v1/controllers/order.controller.js
@@ -35,3 +35,17 @@ export const updateOrderStatus = async (req, res) => {
       errorHandler(error, res);
     }
   };
+
+
+export const getOrders = async (req, res) => {
+    try {
+      const { restaurantId } = req.params;
+  
+      const orders = await orderService.getOrders(restaurantId, req.query);
+  
+      res.json(orders);
+    } catch (error) {
+      errorHandler(error, res);
+    }
+  };
+  
